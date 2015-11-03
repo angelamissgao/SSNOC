0b5bd4bb206c136fabef490236880d9c77485977
Livani Pillay <livanipillay@gmail.com>
2015-11-03 14:32:03 -0800

    Models

---
+++ public/js/models/member.js
@@ -0,0 +1,25 @@
+app.factory('member', function(){
+	
+	function Member(id, username, password, status) {
+    // Public properties, assigned to the instance ('this')
+    this.id = id;
+    this.username = username;
+    this.password = password;
+    this.status = status;
+  }
+
+  Member.prototype.isOnline = function()
+    {
+        if(this.status != 0)
+        {
+          return true;
+        }
+        else
+        {
+          return false;
+        }
+    }
+
+
+  return Member;
+});
