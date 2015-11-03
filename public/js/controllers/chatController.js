f23ecd74be2f4fc0fa96bc336499e1652ebe31a6
Angela Gao <angelagao@Yixuans-Air.sv.cmu.local>
2015-11-02 18:38:01 -0800

    fix search stop words bug

--- public/js/controllers/chatController.js
+++ public/js/controllers/chatController.js
@@ -93,7 +93,6 @@ app.controller("chatController",function($scope, ssnocService, $q,$rootScope){
 
     $rootScope.socket.on('message', function(msg){
         $scope.messages.push(msg);
-        console.log("msg %p",  msg);
         $scope.chatMessage = "";
         $scope.$apply();
 
@@ -139,18 +138,27 @@ app.controller("chatController",function($scope, ssnocService, $q,$rootScope){
         });
     } 
 
+//search functions 
     $scope.searchMessages = function(){
-      ssnocService.searchPublicMessages($scope.searchMessage)
-      .success(function(response){
-        if(response.length == 0){
-            $scope.searchAlert = true;
-            $scope.messages = null;
-        }
-        else{
-          $scope.searchAlert = false;
-          $scope.messages = response;
-        }
-      });
+      var stopwords = ["a","able","about","across","after","all","almost","also","am","among","an","and","any","are","as","at","be","because","been","but","by","can","cannot","could","dear","did","do","does","either","else","ever","every","for","from","get","got","had","has","have","he","her","hers","him","his","how","however","i","if","in","into","is","it","its","just","least","let","like","likely","may","me","might","most","must","my","neither","no","nor","not","of","off","often","on","only","or","other","our","own","rather","said","say","says","she","should","since","so","some","than","that","the","their","them","then","there","these","they","this","tis","to","too","twas","us","wants","was","we","were","what","when","where","which","while","who","whom","why","will","with","would","yet","you","your"];
+      if (stopwords.indexOf($scope.searchMessage) == -1 ) {
+        console.log("here");
+        ssnocService.searchPublicMessages($scope.searchMessage)
+        .success(function(response){
+          if(response.length == 0){
+              $scope.searchAlert = true;
+              $scope.messages = null;
+          }
+          else{
+            $scope.searchAlert = false;
+            $scope.messages = response;
+          }
+        });
+      }
+      else {
+        $scope.searchAlert = true;
+        $scope.messages = null;
+      } 
     }
 
 
