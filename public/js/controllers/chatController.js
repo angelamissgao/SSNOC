53fd5939cd5492492920a2ab186807ce63e13a53
Angela Gao <angelagao@Yixuans-Air.sv.cmu.local>
2015-11-02 19:28:01 -0800

    bug fix serach

--- public/js/controllers/chatController.js
+++ public/js/controllers/chatController.js
@@ -137,23 +137,23 @@ app.controller("chatController",function($scope, ssnocService, $q,$rootScope){
           $scope.announcements = response;
         });
     } 
-
+      
 //search functions 
+var stopwords = ["a","able","about","across","after","all","almost","also","am","among","an","and","any","are","as","at","be","because","been","but","by","can","cannot","could","dear","did","do","does","either","else","ever","every","for","from","get","got","had","has","have","he","her","hers","him","his","how","however","i","if","in","into","is","it","its","just","least","let","like","likely","may","me","might","most","must","my","neither","no","nor","not","of","off","often","on","only","or","other","our","own","rather","said","say","says","she","should","since","so","some","than","that","the","their","them","then","there","these","they","this","tis","to","too","twas","us","wants","was","we","were","what","when","where","which","while","who","whom","why","will","with","would","yet","you","your"];
+
     $scope.searchMessages = function(){
-      var stopwords = ["a","able","about","across","after","all","almost","also","am","among","an","and","any","are","as","at","be","because","been","but","by","can","cannot","could","dear","did","do","does","either","else","ever","every","for","from","get","got","had","has","have","he","her","hers","him","his","how","however","i","if","in","into","is","it","its","just","least","let","like","likely","may","me","might","most","must","my","neither","no","nor","not","of","off","often","on","only","or","other","our","own","rather","said","say","says","she","should","since","so","some","than","that","the","their","them","then","there","these","they","this","tis","to","too","twas","us","wants","was","we","were","what","when","where","which","while","who","whom","why","will","with","would","yet","you","your"];
       if (stopwords.indexOf($scope.searchMessage) == -1 ) {
-        console.log("here");
-        ssnocService.searchPublicMessages($scope.searchMessage)
-        .success(function(response){
-          if(response.length == 0){
-              $scope.searchAlert = true;
-              $scope.messages = null;
-          }
-          else{
-            $scope.searchAlert = false;
-            $scope.messages = response;
-          }
-        });
+          ssnocService.searchPublicMessages($scope.searchMessage)
+          .success(function(response){
+            if(response.length == 0){
+                $scope.searchAlert = true;
+                $scope.messages = null;
+            }
+            else{
+              $scope.searchAlert = false;
+              $scope.messages = response;
+            }
+          });
       }
       else {
         $scope.searchAlert = true;
@@ -163,17 +163,23 @@ app.controller("chatController",function($scope, ssnocService, $q,$rootScope){
 
 
    $scope.searchAnnouncements = function(){
-    ssnocService.searchAnnouncements($scope.searchAnnouncement)
-    .success(function(response){
-      if(response.length == 0){
-            $scope.searchAlert = true;
-            $scope.announcements = null;
-        }
-      else{
-            $scope.announcements = response;
-            $scope.searchAlert = false;
+     if (stopwords.indexOf($scope.searchAnnouncement) == -1 ) {
+        ssnocService.searchAnnouncements($scope.searchAnnouncement)
+        .success(function(response){
+          if(response.length == 0){
+                $scope.searchAlert = true;
+                $scope.announcements = null;
+            }
+          else{
+                $scope.announcements = response;
+                $scope.searchAlert = false;
+          } 
+        });
+      }
+      else {
+        $scope.searchAlert = true;
+        $scope.announcements = null;
       } 
-    });
   }
 
   $scope.searchMemberNames = function(){
